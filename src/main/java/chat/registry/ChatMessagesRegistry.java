4a16e96fc1d340f3691c6652ada14ce05b432b8f
Nour Sadek <nour.sadek@mail.utoronto.ca>
2024-02-03 22:56:44 -0500

    Created registries that save link each websocket session to a user as well as saves all previously inputted messages

---
+++ src/main/java/chat/registry/ChatMessagesRegistry.java
@@ -0,0 +1,20 @@
+package chat.registry;
+
+import chat.model.ChatMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class ChatMessagesRegistry {
+
+    private final List<ChatMessage> chatMessages = Collections.synchronizedList(new ArrayList<>());
+
+    public void registerChatMessage(ChatMessage chatMessage) {
+        chatMessages.add(chatMessage);
+    }
+
+    public List<ChatMessage> getChatMessageRegistry() {
+        return this.chatMessages;
+    }
+}
---
+++ src/main/java/chat/registry/UserSessionRegistry.java
@@ -0,0 +1,35 @@
+package chat.registry;
+
+import chat.model.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class UserSessionRegistry {
+
+    private final Map<String, User> userSessionMap = new ConcurrentHashMap<>();
+
+    public void registerUserSession(User user, String sessionId) {
+        userSessionMap.put(sessionId, user);
+    }
+
+    public User getUserForSession(String sessionId) {
+        return userSessionMap.get(sessionId);
+    }
+
+    public void removeSession(WebSocketSession session) {
+        userSessionMap.remove(session.getId());
+    }
+    public boolean hasUser(String userName) {
+        for (User user: userSessionMap.values()) {
+            if (user.getUserName().equals(userName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
