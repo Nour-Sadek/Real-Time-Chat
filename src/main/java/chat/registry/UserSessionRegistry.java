ec0d061b98d6fde6d984659f5fc073762680e558
Nour Sadek <nour.sadek@mail.utoronto.ca>
2024-02-05 21:41:23 -0500

    Added a list of users and changed methods to modify it accordingly

--- src/main/java/chat/registry/UserSessionRegistry.java
+++ src/main/java/chat/registry/UserSessionRegistry.java
@@ -4,35 +4,60 @@ import chat.model.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 
 @Component
 public class UserSessionRegistry {
 
     private final Map<String, User> userSessionMap = new ConcurrentHashMap<>();
+    private final List<String> allUsers = new ArrayList<>();
 
     public void registerUserSession(User user, String sessionId) {
+
         userSessionMap.put(sessionId, user);
+        allUsers.add(user.getUserName());
+
     }
 
     public User getUserForSession(String sessionId) {
         return userSessionMap.get(sessionId);
     }
 
-    public void removeSession(WebSocketSession session) {
+    public String removeSession(WebSocketSession session) {
+
+        String disconnectedUser = getUserForSession(session.getId()).getUserName();
+        allUsers.remove(disconnectedUser);
         userSessionMap.remove(session.getId());
+
+        return disconnectedUser;
     }
-    public void removeSession(String session) {
+    public String removeSession(String session) {
+
+        String disconnectedUser = getUserForSession(session).getUserName();
+        allUsers.remove(disconnectedUser);
         userSessionMap.remove(session);
+
+        return disconnectedUser;
     }
     public boolean hasUser(String userName) {
-        for (User user: userSessionMap.values()) {
-            if (user.getUserName().equals(userName)) {
-                return true;
-            }
+        return allUsers.contains(userName);
+    }
+
+    public String toString() {
+        String output = "Session Id : UserName\n";
+        for (var entrySet: userSessionMap.entrySet()) {
+            output += entrySet.getKey() + " : " + entrySet.getValue().getUserName() + "\n";
         }
-        return false;
+        return output;
+    }
+
+    public boolean isEmpty() {
+        return userSessionMap.isEmpty();
+    }
+
+    public List<String> getAllUsers() {
+        return allUsers;
     }
 
 }
